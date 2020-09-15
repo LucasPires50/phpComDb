b31572ae0fe6c4f942291b78c9bb3057648bd635
Lucas Pires <luc_50@outlook.com>
2020-09-15 15:51:45 -0300

    first commit

---
+++ Classes/Usuario.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Classes;
+
+require 'Conexao.php';
+use Classes\Conexao;
+
+class Usuario {
+    
+    private $conexao;
+    
+    public function __construct() {
+        $this->conexao = Conexao::getConexao();
+    }
+    
+    public function listar() {
+        
+        $sql = "select codigo, nome, email, login from usuario;";       
+        $q = $this->conexao->prepare($sql);
+        $q->execute();
+        return $q;
+        
+    }
+    
+    public function inserir($nome, $email, $login, $senha) {
+        
+        $sql = "insert into usuario (nome, email, login, senha) values (?, ?, ?, ?);";
+        $q = $this->conexao->prepare($sql);
+        
+        $q->bindParam(1, $nome);
+        $q->bindParam(2, $email);
+        $q->bindParam(3, $login);
+        $q->bindParam(4, md5($senha));
+        $q->bindParam(3, $senha);
+                
+        $q->execute();
+        
+    }
+    
+}
+?>
---
+++ editar.php
@@ -0,0 +1,78 @@
+<?php
+
+use Classes\Editar;
+
+$parametro = filter_input_array(INPUT_GET, FILTER_DEFAULT);
+
+$data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+require './Classes/Editar.php';
+
+$usu = new editarUser();
+
+if (isset($data ['salvar'])) {
+
+    $usu->editar($parametro ['codigo'], $data ['nome'], $data ['email'], $data ['login'], $data ['senha']);
+    header("Location: index.php");
+}
+
+$usuario = $usu->getUsuario($parametro['codigo']);
+?>
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!--Lincando o bootstrat a pagina-->
+        <link href="style/estiloCSS.css" rel="stylesheet" type="text/css"/>
+
+<?php if (isset($data ['salvar'])) { ?>
+            <script>
+                alert("Usuário editado com sucesso !");
+            </script>
+        <?php } ?>
+
+    </head>
+    <body>
+        <div>
+
+            <form action="editar.php?codigo=<?php echo $parametro['codigo']; ?>" method="post" class="letra">
+                <h1>Editar o Formulário</h1>
+
+                <br>
+
+                <label for="nome">Nome:</label>
+                <input type="text" class="input" required name="nome" id="nome" value="<?php echo $usuario ['nome'] ?>"/>
+                <br/>
+
+                <label for="email">E-mail:</label>
+                <input type="email" class="input" name="email" id="email" value="<?php echo $usuario ['email'] ?>" />
+                <br/>
+
+                <label for="login">Login:</label>
+                <input type="text" class="input" required name="login" id="login" value="<?php echo $usuario ['login'] ?>"/>
+                <br/>
+
+                <label for="senha">Senha:</label>
+                <input type="text" class="input"  name="senha" id="senha"/>
+                <br/>
+
+                <label for="senha-confirma">Confirme sua senha:</label>
+                <input type="password" class="input" name="senha-confirma" id="senha-confirma" />
+                <br/>
+
+
+                <button type="submit" class="btn btn-primary" id="salvar" name="salvar">Salvar</button>
+
+            </form> 
+
+        </div>
+    </body>
+</html>
